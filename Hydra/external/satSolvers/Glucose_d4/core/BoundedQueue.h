da80760455d89592ad48027dac7927edf6f1af59
illner <illner3@gmail.com>
2026-07-22 16:59:06 +0200

    Glucose (d4) - change #ifndef

--- Hydra/external/satSolvers/Glucose_d4/core/BoundedQueue.h
+++ Hydra/external/satSolvers/Glucose_d4/core/BoundedQueue.h
@@ -19,8 +19,8 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
-#ifndef BoundedQueue_h
-#define BoundedQueue_h
+#ifndef Glucose_d4_BoundedQueue_h
+#define Glucose_d4_BoundedQueue_h
 
 #include "../mtl/Vec.h"
 
